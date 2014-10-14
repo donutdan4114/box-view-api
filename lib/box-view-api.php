a353fea0c7065348a5ff4c91feb7efdb1837cbe1
Samantha Geitz <samanthamichele7@gmail.com>
2014-10-14 15:47:07 -0500

    Check for type property in httpRequest method to avoid error

--- lib/box-view-api.php
+++ lib/box-view-api.php
@@ -244,7 +244,7 @@ class Box_View_API {
     // Close and return the curl response.
     $result = $this->parseResponse($ch, $response);
     curl_close($ch);
-    if (is_object($result->response) && $result->response->type === 'error') {
+    if (is_object($result->response) && property_exists($result->response, 'type') && $result->response->type === 'error') {
       throw new Box_View_Exception('Error: ' . $result->response->message, $result->headers->code);
     }
     return $result;
